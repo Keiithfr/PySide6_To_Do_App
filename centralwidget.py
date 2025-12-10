02b274bd7595041b718c42f0a57381fc06ef4504
mutumaz <mutumabizz@gmail.com>
2025-12-10 10:53:37 +0300

    create centralwidget module

---
+++ centralwidget.py
@@ -0,0 +1,6 @@
+from PySide6.QtWidgets import QPushButton, QLabel, QLineEdit, QVBoxLayout,QWidget,QListWidget
+
+class to_do_app_centralwidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        
