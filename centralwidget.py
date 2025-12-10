759b141ce6fd58029109d6639575e9d6369dc197
mutumaz <mutumabizz@gmail.com>
2025-12-10 11:06:23 +0300

    add widgets and set layout

--- centralwidget.py
+++ centralwidget.py
@@ -1,6 +1,29 @@
-from PySide6.QtWidgets import QPushButton, QLabel, QLineEdit, QVBoxLayout,QWidget,QListWidget
+from PySide6.QtWidgets import (
+    QPushButton,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget,
+    QListWidget,
+)
+
 
 class to_do_app_centralwidget(QWidget):
     def __init__(self):
         super().__init__()
-        
+        self.initUI()
+
+    def initUI(self):
+        self.add_task_label = QLabel("Enter a Task")
+        self.input_task = QLineEdit()
+        self.input_task.setPlaceholderText("Enter task...")
+        self.add_task_button = QPushButton("Add Task")
+
+        v_layout = QVBoxLayout
+        v_layout.addWidget(self.add_task_label)
+        v_layout.addWidget(self.input_task)
+        v_layout.addWidget(self.add_task_button)
+
+        self.setLayout(v_layout)
+
+
