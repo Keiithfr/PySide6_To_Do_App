b62fd5bebabed0d805a1c073174e506c7745cd89
mutumaz <mutumabizz@gmail.com>
2025-12-18 11:03:06 +0300

    add json persistence

--- centralwidget.py
+++ centralwidget.py
@@ -8,6 +8,8 @@ from PySide6.QtWidgets import (
     QMessageBox,
 )
 from PySide6.QtCore import Qt
+import json
+import os
 
 
 class to_do_app_centralwidget(QWidget):
@@ -44,6 +46,7 @@ class to_do_app_centralwidget(QWidget):
         self.add_task_button.clicked.connect(self.add_task)
         self.input_task.returnPressed.connect(self.add_task)
         self.list.itemDoubleClicked.connect(self.delete_task)
+        self.load_tasks()
 
     def add_task(self):
         task = self.input_task.text().strip()
@@ -53,6 +56,7 @@ class to_do_app_centralwidget(QWidget):
 
         self.list.addItem(task)
         self.input_task.clear()
+        self.save_tasks()
 
     def delete_task(self, item):
         reply = QMessageBox.question(
@@ -64,3 +68,29 @@ class to_do_app_centralwidget(QWidget):
 
         if reply == QMessageBox.Yes:
             self.list.takeItem(self.list.row(item))
+            self.save_tasks()
+
+            # Persistence(saving)
+
+    def get_tasks_path(self):
+        return os.path.join(os.path.dirname(__file__), "tasks.json")
+
+    def save_tasks(self):
+        tasks = [self.list.item(i).text() for i in range(self.list.count())]
+
+        with open(self.get_tasks_path(), "w") as f:
+            json.dump(tasks, f, indent=2)
+
+    def load_tasks(self):
+        path = self.get_tasks_path()
+        if not os.path.exists(path):
+            return
+        try:
+            with open(path, "r") as f:
+                tasks = json.load(f)
+        except json.JSONDecodeError:
+            # if the file is corrupted or empty
+            return
+
+        for task in tasks:
+            self.list.addItem(task)
